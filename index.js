d8a704e803537c160b5d45611b835945f1a12ecd
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-03-15 05:02:25 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(){
+    return (add())
+}
+function subtract(){
+    return (subtract())
+}
+function multiply(){
+    return (multiply())
+}
+function divide(){
+    return (divide())
+}
+function add(a, b){
+    return (a + b)
+}
+function subtract(a, b){
+    return (a - b)
+}
+function multiply(a, b){
+    return (a * b)
+}
+function divide(a, b){
+    return (a / b)
+}
+n = 1
+
+function increment(n) {
+    return (n+= 1);
+  }
+  
+function decrement(n) {
+    return (n-= 1);
+  }
+
+function makeInt(n) {
+    return (parseInt(n, 10)); 
+   
+}
+function preserveDecimal(n) {
+    return(parseFloat(n))
+}
